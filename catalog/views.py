1129321e341a04424a5294544c4c87165880f0c5
Valeriya <chulkovav66@mail.ru>
2025-06-11 19:23:35 +0300

    Django Homework 23

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from catalog.models import Product, ContactInfo
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    latest_products = Product.objects.order_by('-created_at')[:5]
+
+    for product in latest_products:
+        print(product)
+
+    context = {'latest_products': latest_products}
+
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
@@ -11,7 +20,12 @@ def contacts(request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-
-
         return HttpResponse('Данные успешно отправлены!')
-    return render(request, 'catalog/contacts.html')
+
+    contact_info = ContactInfo.objects.all()
+
+    context = {
+        'contact_info': contact_info,
+    }
+
+    return render(request, 'catalog/contacts.html', context)
